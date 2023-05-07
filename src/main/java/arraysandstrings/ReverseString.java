3e4817a462d803fe82a065db6c527b1539d70ed2
Evgenia Chendeva <evgenia.chendeva@proton.me>
2023-05-07 22:29:56 +0300

    task description added to solutions

--- src/main/java/arraysandstrings/ReverseString.java
+++ src/main/java/arraysandstrings/ReverseString.java
@@ -1,5 +1,13 @@
 package arraysandstrings;
 
+    /*
+    Write a function that reverses a string. The input string is given as an array of characters s.
+    You must do this by modifying the input array in-place with O(1) extra memory.
+
+    Input: s = ["h","e","l","l","o"]
+    Output: ["o","l","l","e","h"]
+     */
+
 public class ReverseString {
 
     public void reverseString(char[] s) {
--- src/main/java/arraysandstrings/SquaresOfASortedArray.java
+++ src/main/java/arraysandstrings/SquaresOfASortedArray.java
@@ -2,6 +2,16 @@ package arraysandstrings;
 
 import java.util.Arrays;
 
+    /*
+    Given an integer array nums sorted in non-decreasing order, return an array of the squares
+    of each number sorted in non-decreasing order.
+
+    Input: nums = [-4,-1,0,3,10]
+    Output: [0,1,9,16,100]
+    Explanation: After squaring, the array becomes [16,1,0,9,100].
+    After sorting, it becomes [0,1,9,16,100]
+     */
+
 public class SquaresOfASortedArray {
 
     public int[] sortedSquares(int[] nums) {
