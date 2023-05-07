c63897d7025efe678d61238f2e991b56ec8b4634
Evgenia Chendeva <evgenia.chendeva@proton.me>
2023-05-07 20:23:07 +0300

    Two pointers: Reverse String task

---
+++ src/main/java/twopointers/ReverseString.java
@@ -0,0 +1,21 @@
+package twopointers;
+
+public class ReverseString {
+
+    public void reverseString(char[] s) {
+
+        int left = 0;
+        int right = s.length - 1;
+
+        while(left < right) {
+            char tempChar = s[left];
+
+            s[left] = s[right];
+
+            s[right] = tempChar;
+
+            left++;
+            right++;
+        }
+    }
+}
