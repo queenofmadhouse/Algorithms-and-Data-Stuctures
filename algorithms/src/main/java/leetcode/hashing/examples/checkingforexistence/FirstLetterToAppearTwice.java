459c1cd1524a7e4435ab5892354fa1c926524b8f
Evgenia Chendeva <evgenia.chendeva@proton.me>
2023-05-09 20:24:48 +0300

    Two examples added for hashing chapter

---
+++ algorithms/src/main/java/leetcode/hashing/examples/checkingforexistence/FirstLetterToAppearTwice.java
@@ -0,0 +1,22 @@
+package leetcode.hashing.examples.checkingforexistence;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class FirstLetterToAppearTwice {
+    public char repeatedCharacter(String s) {
+        Set<Character> seen = new HashSet<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char charForStep = s.charAt(i);
+
+            if(seen.contains(charForStep)) {
+                return charForStep;
+            } else {
+                seen.add(charForStep);
+            }
+        }
+
+        return ' ';
+    }
+}
---
+++ algorithms/src/main/java/leetcode/hashing/examples/checkingforexistence/TwoSum.java
@@ -0,0 +1,21 @@
+package leetcode.hashing.examples.checkingforexistence;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class TwoSum {
+
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> dic = new HashMap<Integer, Integer>();
+        for (int i = 0; i < nums.length; i++) {
+            int num = nums[i];
+            int complement = target - num;
+            if (dic.containsKey(complement)) { // This operation is O(1)!
+                return new int[] { i, dic.get(complement) };
+            }
+            dic.put(num, i);
+        }
+
+        return new int[] { -1, -1};
+    }
+}
