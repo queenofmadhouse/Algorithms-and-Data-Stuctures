f2fb8c3d43128b6a11c86815301def3a60f0db9f
Evgenia Chendeva <evgenia.chendeva@proton.me>
2023-05-10 23:26:16 +0300

    Group Anagrams example added

---
+++ algorithms/src/main/java/leetcode/hashing/examples/GroupAnagrams.java
@@ -0,0 +1,30 @@
+package leetcode.hashing.examples;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/*
+    https://leetcode.com/problems/group-anagrams/
+*/
+public class GroupAnagrams {
+
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> groups = new HashMap<>();
+        for (String s: strs) {
+            char[] arr = s.toCharArray();
+            Arrays.sort(arr);
+            String key = new String(arr);
+
+            if (!groups.containsKey(key)) {
+                groups.put(key, new ArrayList<String>());
+            }
+
+            groups.get(key).add(s);
+        }
+
+        return new ArrayList(groups.values());
+    }
+}
