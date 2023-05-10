4f5ef03be01760cf9b6216ff87ee528656644949
Evgenia Chendeva <evgenia.chendeva@proton.me>
2023-05-10 20:17:44 +0300

    Check if All Characters Have Equal Number of Occurrences example added

---
+++ algorithms/src/main/java/leetcode/hashing/examples/counting/CheckIfAllCharactersHaveEqualNumberOfOccurrences.java
@@ -0,0 +1,22 @@
+package leetcode.hashing.examples.counting;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+/*
+    https://leetcode.com/problems/check-if-all-characters-have-equal-number-of-occurrences/
+*/
+public class CheckIfAllCharactersHaveEqualNumberOfOccurrences {
+
+    public boolean areOccurrencesEqual(String s) {
+        Map<Character, Integer> counts = new HashMap<>();
+        for (char c: s.toCharArray()) {
+            counts.put(c, counts.getOrDefault(c, 0) + 1);
+        }
+
+        Set<Integer> frequencies = new HashSet<>(counts.values());
+        return frequencies.size() == 1;
+    }
+}
