50ad64deeb2aedd6910bafca74fa780a83b952ce
Evgenia Chendeva <evgenia.chendeva@proton.me>
2023-05-12 00:41:28 +0300

    Path Crossing example added

---
+++ algorithms/src/main/java/leetcode/hashing/examples/checkingforexistence/PathCrossing.java
@@ -0,0 +1,34 @@
+package leetcode.hashing.examples.checkingforexistence;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class PathCrossing {
+    public boolean isPathCrossing(String path) {
+        int x = 0, y = 0;
+        Set<String> visited = new HashSet<String>();
+        visited.add(x+","+y);
+        for(int i = 0; i < path.length(); i++){
+            if(path.charAt(i) == 'N'){
+                y++;
+            }
+            else if(path.charAt(i) == 'S'){
+                y--;
+            }
+            else if(path.charAt(i) == 'E'){
+                x++;
+            }
+            else if(path.charAt(i) == 'W'){
+                x--;
+            }
+            String temp = x + "," + y;
+            if(visited.contains(temp)){
+                return true;
+            }
+            else{
+                visited.add(temp);
+            }
+        }
+        return false;
+    }
+}
