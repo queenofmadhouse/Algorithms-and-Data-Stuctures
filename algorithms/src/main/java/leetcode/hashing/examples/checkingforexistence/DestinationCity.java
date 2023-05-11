0f54bcad3016781989f8b44e25e813e01f81b323
Evgenia Chendeva <evgenia.chendeva@proton.me>
2023-05-12 00:15:48 +0300

    DestinationCity example add

---
+++ algorithms/src/main/java/leetcode/hashing/examples/checkingforexistence/DestinationCity.java
@@ -0,0 +1,25 @@
+package leetcode.hashing.examples.checkingforexistence;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class DestinationCity {
+    public String destCity(List<List<String>> paths) {
+        Map<String, Integer> map = new HashMap<>();
+
+        for (int i=0; i<paths.size(); i++){
+
+            map.put(paths.get(i).get(0), 1);
+        }
+
+        for (int i = 0; i < paths.size(); i++){
+
+            if (map.get(paths.get(i).get(1)) == null){
+
+                return paths.get(i).get(1);
+            }
+        }
+        return "";
+    }
+}
