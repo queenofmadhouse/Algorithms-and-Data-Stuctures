09c0cd48a6d0cce87efe5c5d9dd9c7866373442e
Evgenia Chendeva <evgenia.chendeva@proton.me>
2023-05-21 22:11:43 +0300

    Middle Of The Linked List example created solving created

---
+++ algorithms/src/main/java/leetcode/linkedlists/tasks/MiddleOfTheLinkedList.java
@@ -0,0 +1,25 @@
+package leetcode.linkedlists.tasks;
+
+public class MiddleOfTheLinkedList {
+
+    public ListNode middleNode(ListNode head) {
+
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        return slow;
+    }
+}
+
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ }
